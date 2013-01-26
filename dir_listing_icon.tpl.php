45e324d931b9a15952310c97f75bc3dafb7ce0a0
Joop Sint Jago <j.sintjago@xs4all.nl>
2013-01-26 15:02:45 +0100

    Editorial - re-saved all files with proper file enc and UNIX EOL char

--- dir_listing_icon.tpl.php
+++ dir_listing_icon.tpl.php
@@ -1,7 +1,7 @@
 <div class='filebrowser-thumbnail'>
-  <div class="fb-img"><?php print $thumbnail ?></div>
+  <?php print $thumbnail ?>
   <div class='title'>
-  	<?php echo $description ?><br/>
+    <?php echo $description ?><br/>
     <a href='<?php print $href?>'><?php echo t('download') ?></a>
     <?php if ($new) print $new ?>
   </div>
